e95a7bf9658aa2139edb399cb1d460bfc8892f0a
moonchild <a-valentina.hirsch@hotmail.com>
2024-07-31 14:40:11 +0200

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="matrixkit",
-    version="0.1.0",
+    version="0.1.1",
     author=["Toni Dieckhoff", "Anna-Valentina Hirsch"],
     author_email="a-valentina.hirsch@hotmail.com",
     description="Synthetic Matrix Generation for Machine Learning and Scientific Computing",
