24cc5bde5885413e7322adc5403386808e35e801
moonchild <a-valentina.hirsch@hotmail.com>
2024-07-31 15:57:49 +0200

    update license

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License 2.0",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
